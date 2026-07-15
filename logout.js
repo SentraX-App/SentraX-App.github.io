a9e2efb9d1a20e72934d4b25970686f948156b8b
SentraX-App <alecedoh1994@gmail.com>
2026-07-15 19:55:19 +0100

    Create logout.js

---
+++ logout.js
@@ -0,0 +1,33 @@
+import { auth } from "./firebase.js";
+
+import {
+signOut
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+const button = document.getElementById("logoutBtn");
+
+
+if(button){
+
+button.onclick = async()=>{
+
+try{
+
+await signOut(auth);
+
+window.location.href="auth.html";
+
+}
+
+catch(error){
+
+alert(error.message);
+
+}
+
+};
+
+}
