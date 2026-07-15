05ae361c96bc8f4c70574bfb86c098522fc89de2
SentraX-App <alecedoh1994@gmail.com>
2026-07-15 20:56:01 +0100

    Update protect.js

--- protect.js
+++ protect.js
@@ -1,17 +1,51 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 
 import {
 onAuthStateChanged
-}
-from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+doc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
-onAuthStateChanged(auth, (user)=>{
+onAuthStateChanged(auth, async (user)=>{
 
 if(!user){
 
 window.location.href="auth.html";
+return;
+
+}
+
+
+// Check user profile
+
+const userRef = doc(db, "users", user.uid);
+
+const userSnap = await getDoc(userRef);
+
+
+if(!userSnap.exists()){
+
+// New user
+// Allow onboarding page
+
+localStorage.removeItem("userName");
+
+document.body.style.display = "block";
+
+}else{
+
+// Existing user
+// Load profile
+
+const data = userSnap.data();
+
+localStorage.setItem("userName", data.name || "");
+
+localStorage.setItem("userCondition", data.condition || "");
 
 }
 
