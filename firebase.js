aab1467cc37195a71be3c15b553c98ffc0ebaee0
SentraX-App <alecedoh1994@gmail.com>
2026-07-15 19:26:02 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,55 @@
+// Sentra-X Firebase Configuration
+// Firebase Modular SDK
+
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { 
+getAuth 
+} from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+getFirestore
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+// Your Firebase Configuration
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyCHeWvZzGZVa79m2JvvpkX5Xlvmj8vpdPc",
+
+authDomain: "sentra-x.firebaseapp.com",
+
+projectId: "sentra-x",
+
+storageBucket: "sentra-x.firebasestorage.app",
+
+messagingSenderId: "5654480364",
+
+appId: "1:5654480364:web:bd6dbe766a1c46edb66cc9"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+
+// Authentication
+
+const auth = getAuth(app);
+
+
+// Database
+
+const db = getFirestore(app);
+
+
+// Export
+
+export { auth, db };
