5c27335069e8a12fba084cd953ce69f454eb367b
SentraX-App <alecedoh1994@gmail.com>
2026-07-17 10:04:09 +0100

    Update protect.js

--- protect.js
+++ protect.js
@@ -1,25 +1,13 @@
 import { auth } from "./firebase.js";
-
-import {
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-let checked = false;
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
 
-  if (user) {
-
-    console.log("User authenticated:", user.email);
-
-  } else {
-
-    if (checked) {
-      window.location.replace("auth.html");
-    }
-
+  if (!user) {
+    window.location.replace("auth.html");
+    return;
   }
 
-  checked = true;
+  console.log("Authenticated:", user.email);
 
 });
