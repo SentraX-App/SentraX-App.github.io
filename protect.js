1cb6171cdbce0b2691273596de05918f5ba07221
SentraX-App <alecedoh1994@gmail.com>
2026-07-17 10:30:02 +0100

    Update protect.js

--- protect.js
+++ protect.js
@@ -1,8 +1,17 @@
 import { auth } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
 
+  // Already on login page?
+  if (window.location.pathname.includes("auth.html")) {
+    return;
+  }
+
+  // Not logged in
   if (!user) {
     window.location.replace("auth.html");
     return;
